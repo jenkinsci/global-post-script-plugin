9efd4990827a316273d2fc1b3ddcffd6317ae179
orctom <orctom@gmail.com>
2014-07-01 19:43:33 +0800

    icon

--- src/test/resources/downstream_job_trigger.groovy
+++ src/test/resources/downstream_job_trigger.groovy
@@ -1,19 +1,19 @@
 def triggers = [
-    basepom  : {
-        manager.triggerJob("basepom")
-        manager.triggerRemoteJob("http://localhost:8080/job/basepom85/build?delay=0sec")
-    },
-    basepom85: {
-        manager.triggerRemoteJob("http://localhost:8080/job/basepom85/build?delay=0sec")
-    }
+        basepom  : {
+            manager.triggerJob("basepom")
+            manager.triggerRemoteJob("http://localhost:8080/job/basepom85/build?delay=0sec")
+        },
+        basepom85: {
+            manager.triggerRemoteJob("http://localhost:8080/job/basepom85/build?delay=0sec")
+        }
 ]
 
 if ("true" == deploy && deploy_targets?.trim()) {
-    manager.addBadge("computer.png", deploy_targets)
+    manager.addBadge("computer.png", "[DEV: " + deploy_targets + "]")
 }
 if ("true" == dropdeploy && dropdeploy_targets?.trim()) {
     dropdeploy_targets.split(',').each {
         triggers[it]()
     }
-    manager.addBadge("server.png", dropdeploy_targets)
+    manager.addBadge("server.png", "[SQ: " + dropdeploy_targets + "]")
 }
