78a4dc51c99c370d96317216a62dd31bc6f1e30d
Mark Waite <mark.earl.waite@gmail.com>
2022-02-26 20:35:10 -0700

    Use UTF-8 character set

--- src/main/java/com/orctom/jenkins/plugin/globalpostscript/runner/ShellScriptRunner.java
+++ src/main/java/com/orctom/jenkins/plugin/globalpostscript/runner/ShellScriptRunner.java
@@ -7,6 +7,7 @@ import org.codehaus.plexus.util.FileUtils;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.Map;
 
 import static com.orctom.jenkins.plugin.globalpostscript.ScriptContentLoader.getScriptContent;
@@ -39,7 +40,7 @@ public class ShellScriptRunner extends ScriptRunner {
       }
       Process process = builder.start();
 
-      try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+      try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream(), StandardCharsets.UTF_8))) {
         String line;
         while ((line = reader.readLine()) != null) {
           println(listener, line);
